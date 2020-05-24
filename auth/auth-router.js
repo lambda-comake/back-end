fc13064491022aab5c05f14dfa3280a9b1f7de66
Tanner Williams <tannerwill756@gmail.com>
2020-05-24 17:38:38 -0400

    C:/Program Files/Git/register working

---
+++ auth/auth-router.js
@@ -0,0 +1,44 @@
+const router = require("express").Router();
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const Users = require("../user/user-model.js");
+
+router.post("/register", (req, res) => {
+  const credentials = req.body;
+
+  if (isValid(credentials)) {
+    const hash = bcryptjs.hashSync(credentials.password, 8);
+    credentials.password = hash;
+
+    Users.add(credentials)
+      .then((user) => {
+        res.status(201).json({ data: user });
+      })
+      .catch((err) => {
+        res.status(500).json({ err: err.message });
+      });
+  } else {
+    res.status(400).json({
+      message: "Please provide username and password",
+    });
+  }
+});
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  };
+  const secret = process.env.JWT_secret || "supersecret";
+  const options = {
+    expiresIn: "1d",
+  };
+  return jwt.sign(payload, secret, options);
+}
+
+function isValid(user) {
+  return Boolean(user.username && user.password);
+}
+
+module.exports = router;
