d350623bfb7c8b31c9f325b2ecd5367fded20a04
Tanner Williams <tannerwill756@gmail.com>
2020-05-24 13:47:41 -0400

    get issue by id

--- issues/issue-model.js
+++ issues/issue-model.js
@@ -2,8 +2,13 @@ const db = require("../data/db-config.js");
 
 module.exports = {
   getAllIssues,
+  getIssuesById,
 };
 
 function getAllIssues() {
   return db("issues");
+}
+
+function getIssuesById(id) {
+  return db("issues").where({ id }).first();
 }
--- issues/issue-router.js
+++ issues/issue-router.js
@@ -5,8 +5,25 @@ const Issues = require("./issue-model.js");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  Issues.getAllIssues().then((issues) => {
-    res.status(200).json(issues);
+  Issues.getAllIssues()
+    .then((issues) => {
+      res.status(200).json(issues);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "issue retrieving issues data" });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  Issues.getIssuesById(req.params.id).then((issues) => {
+    console.log("get by id", issues);
+    if (issues) {
+      res.status(200).json(issues);
+    } else {
+      res
+        .status(404)
+        .json({ message: "Couldn't find any issues with that ID" });
+    }
   });
 });
 
