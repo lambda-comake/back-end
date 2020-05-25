eecfedb98edc42830e2c15dae176ded2a57ef845
Tanner Williams <tannerwill756@gmail.com>
2020-05-25 18:26:45 -0400

    knex stuff

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,5 @@
 // Update with your config settings.
+const connectionString = process.env.DATABASE_URL;
 
 module.exports = {
   development: {
@@ -38,17 +39,16 @@ module.exports = {
 
   production: {
     client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
-    },
+    connection: connectionString,
     pool: {
       min: 2,
       max: 10,
     },
     migrations: {
-      tableName: "knex_migrations",
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
     },
   },
 };
