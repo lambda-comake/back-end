d0a3082ef78c3cbed6751b48cdf88c1fffffa49a
Tanner Williams <tannerwill756@gmail.com>
2020-05-25 18:36:23 -0400

    fixed seed for deployment

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -1,10 +1,6 @@
 exports.seed = function (knex) {
-  return knex("users")
-    .truncate()
-    .then(function () {
-      return knex("users").insert([
-        { id: 1, username: "corkcicle", password: "123" },
-        { id: 2, username: "water", password: "pass" },
-      ]);
-    });
+  return knex("users").insert([
+    { id: 1, username: "corkcicle", password: "123" },
+    { id: 2, username: "water", password: "pass" },
+  ]);
 };
